c603d0a140901cd5af7090a704f760f20736f87e
wirapratamaz <wirabagus185@gmail.com>
2024-02-01 12:38:27 +0800

    Add status field to Book struct and implement Status enum

--- structs-enums/src/main.rs
+++ structs-enums/src/main.rs
@@ -13,9 +13,20 @@ enum Status {
     Borrowed
 }
 
+impl Status {
+    // method to get string representation of a Status
+    fn as_str(&self) -> &'static str {
+        match *self {
+            Status::Available => "available",
+            Status::Sold => "sold",
+            Status::Borrowed => "borrowed",
+        }
+    }
+}
+
 impl Book {
     // associate function
-    fn new(title: &str, author: &str, pages: u32, price: f32) -> Book {
+    fn new(title: &str, author: &str, pages: u32, price: f32, status: Status) -> Book {
         Book {
             title: title.to_string(),
             author: author.to_string(),
@@ -26,13 +37,13 @@ impl Book {
 
     // method to get string representation of a Book
     fn get_info(&self) -> String {
-        format!("{} is written by {} and has {} pages. Price is ${:.2}", self.title, self.author, self.pages, self.price)
+        format!("{} is written by {} and has {} pages. Price is ${:.2}. Status: {}", self.title, self.author, self.pages, self.price, self.status.as_str())
     }
 }   
 
 fn main(){
     // create a struct instance
-    let book = Book::new("The Odyssey", "Homer", 608, 9.99);
+    let book = Book::new("The Odyssey", "Homer", 608, 9.99, Status::Available);
     // print the book info
     println!("{}", book.get_info());
 }
