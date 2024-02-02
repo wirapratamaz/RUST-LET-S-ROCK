d132a5e5b8a8d652f6c5531c2a0128c879500fc1
wirapratamaz <wirabagus185@gmail.com>
2024-02-02 10:24:40 +0800

    Update with full candid and implementation simple canister

--- candid/src/main.rs
+++ candid/src/main.rs
@@ -12,11 +12,27 @@
 // import ic_cdk to use candid macro
 use ic_cdk::export::candid::{CandidType, Deserialize};
 use ic_cdk_macros::*;
+use std::collections::HashMap;
 
 // define Profile struct
 #[derive(CandidType, Deserialize)]
 struct Greeting {
     name: String,
+}
+
+// define State struct
+#[derive(Default)]
+struct State {
+    profiles: HashMap<String, String>,
+}
+
+// initialize the state of the canister
+#[init]
+fn init(){
+    let state = State {
+        greetings: HashMap::new(),
+    };
+    ic_cdk::storage::put(state);
 }
 
 // Update functions can modify the state of the canister
@@ -31,6 +47,18 @@ fn greet(g: Greeting) -> String {
 // define hello_world query function
 fn hello_world() -> String {
     "Hello, world!".to_string()
+}
+
+// can modify the state of the canister
+fn set_greeting(g: Greeting, greeting: String){
+    let state = ic_sdk::storage::get_mut::<State>();
+    state.greetings.insert(g.name, greeting);
+}
+
+// cannot modify the state of the canister
+fn get_greeting(g:Greeting) -> Option<String>{
+    let state = ic_sdk::storage::get::<State>();
+    state.greetings.get(&g.name).cloned()
 }
 
 // define main function
