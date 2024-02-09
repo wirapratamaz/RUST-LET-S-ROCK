a2cc086418900cc229fa56ab4837e4f2281698b4
wirapratamaz <89260515+wirapratamaz@users.noreply.github.com>
2024-02-09 12:00:51 +0800

    feat: add memory.rs in quickstart

--- stable-structure/src/memory.rs
+++ stable-structure/src/memory.rs
@@ -1,90 +1,39 @@
-use ic_stable_structure::memory_manager::{MemoryManager, MemoryId, VirtualMemory};
-use ic_stable_structure::DefaultMemoryImpl;
+// Import necessary modules and types from the `ic_stable_structures` crate.
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::DefaultMemoryImpl;
 use std::cell::RefCell;
 
-#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
-struct State {
-    // Equals to `true` while the [build_index] task runs.
-    is_build_index_running: bool,
+// Define constants for different memory IDs. These are unique identifiers for different
+// memory areas that we want to manage.
+const UPGRADES: MemoryId = MemoryId::new(0);
+const STABLE_BTREE: MemoryId = MemoryId::new(1);
 
-    /// The principal of the ledger canister that is indexed by this index.
-    ledger_id: Principal,
+// Define the type alias `Memory` for the `VirtualMemory` that uses `DefaultMemoryImpl`.
+// This makes it easier to refer to this specific type of virtual memory throughout the code.
+pub type Memory = VirtualMemory<DefaultMemoryImpl>;
 
-    /// The maximum number of transactions returned by [get_blocks].
-    max_blocks_per_response: u64,
-}
-
-// Memory for upgrades, where data from the heap can be serialized and deserialized
-const UPGRADE_MEMORY: MemoryId = MemoryId::new(0);
-
-// Memory for the stable memory manager using StableBTreeMap
-const STABLE_MEMORY: MemoryId = MemoryId::new(1);
-
-pub type MemoryManager = VirtualMemory<DefaultMemoryImpl>;
-
-// The memory manager is used for simulating multiple memories. Given a `MemoryId` it can
-// return a memory that can be used by stable structures.
+// Declare a thread-local variable `MEMORY_MANAGER`. This variable will be unique to each thread
+// and will hold our memory manager instance. We use `RefCell` to allow for interior mutability,
+// which means we can modify the memory manager even if we have a shared reference to it.
 thread_local! {
-    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl> = RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
 }
 
-pub fn with_memory_manager<F, R>(f: F) -> R
-where
-    F: FnOnce(&MemoryManager<DefaultMemoryImpl>) -> R,
-{
-    MEMORY_MANAGER.with(|memory_manager| f(&memory_manager.borrow()))
-}
-
-pub fn with_memory_manager_mut<F, R>(f: F) -> R
-where
-    F: FnOnce(&mut MemoryManager<DefaultMemoryImpl>) -> R,
-{
-    MEMORY_MANAGER.with(|memory_manager| f(&mut memory_manager.borrow_mut()))
-}
-
-pub fn with_upgrade_memory<F, R>(f: F) -> R
-where
-    F: FnOnce(&MemoryManager<DefaultMemoryImpl>) -> R,
-{
-    with_memory_manager(|memory_manager| {
-        memory_manager.with_memory(UPGRADE_MEMORY, f)
+// Define a function to get the virtual memory instance for upgrades.
+// This function uses the `MEMORY_MANAGER` to retrieve the memory associated with the `UPGRADES` ID.
+pub fn get_upgrades_memory() -> Memory {
+    MEMORY_MANAGER.with(|manager_ref| {
+        // Borrow the memory manager immutably to get the memory instance.
+        manager_ref.borrow().get(UPGRADES)
     })
 }
 
-pub fn get_upgrades_memory() -> MemoryManager<DefaultMemoryImpl> {
-    with_memory_manager(|memory_manager| memory_manager.get_memory(UPGRADE_MEMORY))
-}
-
-pub fn get_stable_btree_memory() -> MemoryManager<DefaultMemoryImpl> {
-    with_memory_manager(|memory_manager| memory_manager.get_memory(STABLE_MEMORY))
-}
-
-pub fn with_stable_btree_memory<F, R>(f: F) -> R
-where
-    F: FnOnce(&MemoryManager<DefaultMemoryImpl>) -> R,
-{
-    with_memory_manager(|memory_manager| {
-        memory_manager.with_memory(STABLE_MEMORY, f)
+// Define a function to get the virtual memory instance for the stable B-tree.
+// This function is similar to `get_upgrades_memory` but retrieves the memory for the `STABLE_BTREE` ID.
+pub fn get_stable_btree_memory() -> Memory {
+    MEMORY_MANAGER.with(|manager_ref| {
+        // Borrow the memory manager immutably to get the memory instance.
+        manager_ref.borrow().get(STABLE_BTREE)
     })
-}
-
-pub fn with_blocks<R>(f: impl FnOnce(&mut MemoryManager<DefaultMemoryImpl>) -> R) -> R {
-    with_memory_manager_mut(|memory_manager| f(memory_manager))
-}
-
-fn with_state<R>(f: impl FnOnce(&State) -> R) -> R {
-    STATE
-        .with(|cell| f(&*cell.borrow()))
-        .expect("Failed to access state")
-}
-
-fn change_state(f:impl FnOnce(&mut State)){
-    STATE
-        .with(|cell| {
-            let mut borrowed = cell.borrow_mut();
-            let mut state = *borrowed.get();
-            f(&mut state);
-            borrowed.set(state);
-        })
-        .expect("Failed to change state");
 }
